90e77b334e7808fb2eb19e1c8c86ec61eeed2c4b
nikonpavlenko <nikonpavlenko@gmail.com>
2023-02-22 00:44:49 +1100

    update

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -2,11 +2,17 @@ package koschei.models;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Duck5 {
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
+    }
+
+    private Egg6 egg;
+
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
     }
 }
